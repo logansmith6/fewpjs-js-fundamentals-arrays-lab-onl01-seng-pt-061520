b66a2374208eb7e11a95dafade69f41f9fe29209
Logan Smith <logsmith06@gmail.com>
2020-12-10 17:39:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your solution here!
-pets.push('Odie');
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push('Odie');
+prepend.unshift('Odie');
